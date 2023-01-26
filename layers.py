90e0ba429f3ca092bc04372955b1528c1d3ee389
snehalstomar <cooldudesnehal1@gmail.com>
2023-01-26 14:07:33 +0530

    fixed_indentation

--- layers.py
+++ layers.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 
 
 def disp_to_depth(disp, min_depth, max_depth):
-       min_disp = 1 / max_depth
+    min_disp = 1 / max_depth
     max_disp = 1 / min_depth
     scaled_disp = min_disp + (max_disp - min_disp) * disp
     depth = 1 / scaled_disp
